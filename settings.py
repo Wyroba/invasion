01545a1738af1d2f41b4b5b7280566c1e64dcfc9
Peter <wyroba.p@gmail.com>
2022-10-06 11:20:37 -0400

    Added Settings class

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """A class to store all settings for Alient Invasion"""
+    def __init__(self):
+        """Initialize the games's settings."""
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
