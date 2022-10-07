0be1316861db61cd40153cbdffb44775be79f491
Peter <wyroba.p@gmail.com>
2022-10-07 10:16:47 -0400

    Added alien class

--- settings.py
+++ settings.py
@@ -4,13 +4,14 @@ class Settings:
         """Initialize the games's settings."""
         self.screen_width = 1000
         self.screen_height = 600
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
 
         # Ship settings
         self.ship_speed = .25
 
         # Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = .75
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 3
