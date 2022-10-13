7f2e8948c749e2ad69384e65dba27b49cde61043
Peter <wyroba.p@gmail.com>
2022-10-13 14:13:46 -0400

    Added all other features including all on screen graphics. Should be mostly complete

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 class Settings:
     """A class to store all settings for Alient Invasion"""
     def __init__(self):
-        """Initialize the games's settings."""
+        """Initialize the games's static settings."""
         self.screen_width = 1000
         self.screen_height = 600
         self.bg_color = (0, 0, 0)
 
         # Ship settings
-        self.ship_speed = .50
+        self.ship_speed = 1
         self.ship_limit = 3
 
         # Bullet settings
@@ -18,7 +18,35 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien Settings
-        self.alien_speed = 5
-        self.fleet_drop_speed = 5
+        self.alien_speed = .5
+        self.fleet_drop_speed = 10
         # Fleet_direction of 1 represents right; -1 is left
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        # How quickly the alien point values incease
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = .5
+        self.bullet_speed = 3
+        self.alien_speed = .5
+
+        # Fleet_direction of 1 represents right; -1 is left
+        self.fleet_direction = 1
+
+        # Scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings and alien values."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
